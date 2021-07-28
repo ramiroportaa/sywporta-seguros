cbfa1404723b2980a988d3745ab73cccff9d126e
ramiporta <p0rt4csd@gmail.com>
2021-07-27 23:36:58 -0300

    preuba de funcionamiento formulario php

--- html/enviar2.php
+++ html/enviar2.php
@@ -1,5 +1,5 @@
 <?php
-// Conecto mi form con el server a traves de los name
+
 $nya_asegurado = $_POST['nya_asegurado'];
 $dni_asegurado = $_POST['dni_asegurado'];
 $tel_asegurado = $_POST['tel_asegurado'];
@@ -18,16 +18,12 @@ $compa_tercero = $_POST['compa_tercero'];
 $fechahora = $_POST['fechahora'];
 $lugar = $_POST['lugar'];
 $descripcion = $_POST['descripcion'];
-$newsletter = $_POST['newsletter'];
 
-​
-// Como funciona con el server
-$header = "From: " . $mail_asegurado . " \r\n";
+$header = 'From: ' . $mail_asegurado . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 ​
-// Como me a llegar el cuerpo del mail a mi, o sea lo que la gente escribió en el form
 $mensaje = "Este mensaje fue enviado por el ASEGURADO " . $nya_asegurado . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail_asegurado . " \r\n";
 $mensaje .= "Su tel es: " . $tel_asegurado . " \r\n";
@@ -47,14 +43,13 @@ $mensaje .= "Fecha y hora del siniestro: " . $fechahora . " \r\n";
 $mensaje .= "Lugar del siniestro: " . $lugar . " \r\n";
 $mensaje .= "Descripcion: " . $_POST['descripcion'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
-$mensaje .= "Suscribe al newletter: " . $newsletter . " \r\n";
 ​
-$para = 'p0rt4csd@gmail.com'; // El mail a donde van a llegar los mensajes
+$para = 'p0rt4csd@gmail.com';
 $asunto = 'Siniestro informado desde la web';
 ​
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
 echo "<h2>Siniestro informado con exito!</h2>​";
-​header("Location:index.html");
+​header("Location: index.html");
 
 ?>
